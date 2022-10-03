bacffb1daecd9ecda2142269d2d7e765cfa8b5f7
Ruba <rubaaboissa@gmail.com>
2022-10-03 13:15:22 +0200

    first task done

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -6,12 +6,60 @@ public class App {
 
     //todo Task 1
     public void largestNumber(){
+       /* Scanner scanner = new Scanner(System.in);
         // input your solution here
+        int counter = 1;
+        double temp = 0;
+
+        System.out.print("Number "+ counter++ + ": ");
+
+        double number = scanner.nextDouble();
+
+
+        temp =  number;
+
+
+        if(number < 0) {
+            System.out.println("No number entered.");
+
+        }
+        while(number  > 0){
+            System.out.print("Number "+ (counter++) + ": ");
+            number = scanner.nextDouble();
+
+            if(number > temp){
+                temp =  number;
+            }
+        }
+
+        if (number == 0 || number < 0){
+
+            System.out.println("The largest number is "+  String.format("%.2f", temp));
+
+        }*/
     }
 
     //todo Task 2
     public void stairs(){
         // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+
+
+        for(int i=0; i< length ; i++) {
+            i++;
+
+            for (int y = 0; y < i; y++){
+
+                System.out.println( y++);
+            }
+        }
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j >= i; j--) {
+                System.out.print(j);
+            }
+            System.out.println(i);
+        }
     }
 
     //todo Task 3
